63ac0888fc375bcdadfb931c73b5dd451be199f3
dusk <52311860+MC-dusk@users.noreply.github.com>
2022-11-12 16:02:22 +0000

    cin getline to get

--- acm/w11/w11q1.cpp
+++ acm/w11/w11q1.cpp
@@ -7,7 +7,7 @@ int main(){
     char str[100];
     // string str;
     // scanf("%s", str);
-    cin.getline(str,100);
+    cin.get(str,100);
     cout << strlen(str) << endl;
     // cout << str.size() << endl;
     return 0;
